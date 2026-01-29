e7a10d4f10ba6e4cb37fc1cdc12d9d4ea33bc0b2
Tanish Hande <shande123@gmail.com>
2026-01-29 12:19:02 +0530

    Hii this is inital commit

---
+++ YCCECalc/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace YCCECalc;
+
+public class MyMathClass
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public double Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Division by zero is not allowed.");
+        }
+        return (double)a / b;
+    }
+}
